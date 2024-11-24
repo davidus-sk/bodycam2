02d87bb13b196df3707e957b8bf7cf90c21207e1
David <travel@davidus.sk>
2024-11-24 13:22:59 -0500

    Restart script

---
+++ camera/scripts/restart.php
@@ -0,0 +1,53 @@
+#!/usr/bin/php
+<?php
+
+// run only once
+$lockFile = fopen('/tmp/camera_restart.pid', 'c');
+$gotLock = flock($lockFile, LOCK_EX | LOCK_NB, $wouldBlock);
+if ($lockFile === false || (!$gotLock && !$wouldBlock)) {
+        throw new Exception("Can't obtain lock.");
+} else if (!$gotLock && $wouldBlock) {
+        exit();
+}//if
+
+ftruncate($lockFile, 0);
+fwrite($lockFile, getmypid() . "\n");
+
+// load libraries
+require(dirname(__FILE__) . '/../../../vendor/autoload.php');
+
+use \PhpMqtt\Client\MqttClient;
+use \PhpMqtt\Client\ConnectionSettings;
+
+// MQTT settings
+$server   = '951badeefd764316aa971d7958e80e0c.s1.eu.hivemq.cloud';
+$port     = 8883;
+$clientId = 'camera-' . trim(`/usr/bin/cat /proc/cpuinfo | /usr/bin/grep "Serial" | /usr/bin/xargs | /usr/bin/cut -d ' ' -f 3`);
+$username = 'marek';
+$password = 'Mqtt12345';
+$clean_session = false;
+$mqtt_version = MqttClient::MQTT_3_1;
+
+// MQTT connection string
+$connectionSettings = (new ConnectionSettings)
+  ->setUsername($username)
+  ->setPassword($password)
+  ->setKeepAliveInterval(60)
+  ->setConnectTimeout(3)
+  ->setLastWillTopic("camera/{$clientId}/last-will")
+  ->setLastWillMessage('client disconnect')
+  ->setUseTls(true)
+  ->setLastWillQualityOfService(0);
+
+// connect to the server
+$mqtt = new MqttClient($server, $port, $clientId, $mqtt_version);
+$mqtt->connect($connectionSettings, $clean_session);
+
+$mqtt->subscribe("camera/{$clientId}/restart", function ($topic, $message) {
+	echo date('r') . "> Restarting camera streamer.\n";
+
+	`/usr/bin/pkill -9 -f "pi_webrtc"`;
+	`/app/bodycam2/camera/stream/pi_webrtc --use_libcamera --fps=10 --width=1280 --height=960 --hw_accel --no_audio --mqtt_host={$server} --mqtt_port={$port} --mqtt_username={$username} --mqtt_password={$password} --uid={$clientId} >> /tmp/pi_webrtc.log 2>&1 &`;
+}, 0);
+
+$mqtt->loop(true);
