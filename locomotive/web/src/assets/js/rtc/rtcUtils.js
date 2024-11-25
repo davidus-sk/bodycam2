617f60317ea5711a380ae655bb5b21f3265abcb9
Marek Drugac <marek@luceon.us>
2024-11-25 15:50:59 +0100

    dev

--- locomotive/web/src/assets/js/rtc/rtcUtils.js
+++ locomotive/web/src/assets/js/rtc/rtcUtils.js
@@ -1,15 +1,25 @@
 export class RtcMessage {
     constructor(type, message) {
         this.type = type;
-        this.message = typeof message === "string" ? message : String(message);
+        this.message = typeof message === 'string' ? message : String(message);
     }
 }
 
 export class MetaCmdMessage {
-    constructor(command, message = "") {
+    constructor(command, message = '') {
         this.command = command;
         this.message = message;
     }
+}
+
+export function sortByMimeTypes(codecs, preferredOrder) {
+    return codecs.sort((a, b) => {
+        const indexA = preferredOrder.indexOf(a.mimeType);
+        const indexB = preferredOrder.indexOf(b.mimeType);
+        const orderA = indexA >= 0 ? indexA : Number.MAX_VALUE;
+        const orderB = indexB >= 0 ? indexB : Number.MAX_VALUE;
+        return orderA - orderB;
+    });
 }
 
 /**
@@ -20,30 +30,23 @@ export class MetaCmdMessage {
  */
 export function removeCodec(orgsdp, codec) {
     const codecRegex = new RegExp(`a=rtpmap:(\\d*) ${codec}/90000\\r\\n`);
-    let modifiedSdp = orgsdp.replace(codecRegex, "");
+    let modifiedSdp = orgsdp.replace(codecRegex, '');
 
     // Remove associated rtcp-fb, fmtp, and apt lines
-    modifiedSdp = modifiedSdp.replace(
-        new RegExp(`a=rtcp-fb:(\\d*) ${codec}.*\\r\\n`, "g"),
-        ""
-    );
-    modifiedSdp = modifiedSdp.replace(
-        new RegExp(`a=fmtp:(\\d*) ${codec}.*\\r\\n`, "g"),
-        ""
-    );
+    modifiedSdp = modifiedSdp.replace(new RegExp(`a=rtcp-fb:(\\d*) ${codec}.*\\r\\n`, 'g'), '');
+    modifiedSdp = modifiedSdp.replace(new RegExp(`a=fmtp:(\\d*) ${codec}.*\\r\\n`, 'g'), '');
 
     // Handle fmtp apt
     const aptRegex = new RegExp(`a=fmtp:(\\d*) apt=(\\d*)\\r\\n`);
-    modifiedSdp = modifiedSdp.replace(aptRegex, "");
+    modifiedSdp = modifiedSdp.replace(aptRegex, '');
 
     // Process video line modifications
     const videoLineRegex = /m=video.*\r\n/;
     const videoLineMatch = modifiedSdp.match(videoLineRegex);
     if (videoLineMatch) {
         let videoLine = videoLineMatch[0].trim();
-        const videoElements = videoLine.split(" ");
-        videoLine =
-            videoElements.filter((el) => el !== codec).join(" ") + "\r\n";
+        const videoElements = videoLine.split(' ');
+        videoLine = videoElements.filter(el => el !== codec).join(' ') + '\r\n';
         modifiedSdp = modifiedSdp.replace(videoLineRegex, videoLine);
     }
 
@@ -56,7 +59,7 @@ export function removeCodec(orgsdp, codec) {
  * @returns {string} - The resulting string.
  */
 export function arrayBufferToString(buffer) {
-    return buffer.reduce((acc, curr) => acc + String.fromCharCode(curr), "");
+    return buffer.reduce((acc, curr) => acc + String.fromCharCode(curr), '');
 }
 
 /**
