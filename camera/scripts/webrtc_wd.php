7d9cf1ae32256e63d13e1c32db287e6fbef53eaf
davidus-sk <travel@davidus.sk>
2024-12-15 18:20:38 -0500

    Update webrtc_wd.php

--- camera/scripts/webrtc_wd.php
+++ camera/scripts/webrtc_wd.php
@@ -1,33 +1,20 @@
 #!/usr/bin/php
 <?php
 
-// run only once
-$lockFile = fopen('/tmp/camera_webrtc_wd.pid', 'c');
-$gotLock = flock($lockFile, LOCK_EX | LOCK_NB, $wouldBlock);
-if ($lockFile === false || (!$gotLock && !$wouldBlock)) {
-        throw new Exception("Can't obtain lock.");
-} else if (!$gotLock && $wouldBlock) {
-        exit();
-}//if
+// load libraries
+require(dirname(__FILE__) . '/../../common/functions.php');
 
-ftruncate($lockFile, 0);
-fwrite($lockFile, getmypid() . "\n");
+// run once
+run_once('/tmp/camera_webrtc_wd.pid', $fh);
 
-// load up config file
-$conf_file = '/app/bodycam2/camera/conf/config.json';
+// load settings
+$config = read_config();
 
-if (!file_exists($conf_file)) {
-	echo date('r') . "> Config file does not exist.\n";
-	exit;
-}//if
+// log
+openlog("camera_webrtc_wd", LOG_PID | LOG_PERROR, LOG_LOCAL0);
 
-$json = file_get_contents($conf_file);
-$data = json_decode($json, TRUE);
-
-if (empty($data)) {
-	echo date('r') . "> Config file is empty.\n";
-	exit;
-}//if
+// globals
+$k = 0;
 
 // keep checking
 while (TRUE) {
@@ -38,17 +25,29 @@ while (TRUE) {
 	if (empty($pid)) {
 		$clientId = 'device-' . trim(`{$data['client_id']}`);
 
-		echo date('r') . "> Starting pi_webrtc for {$clientId}.\n";
-
+		// launch
+		syslog(LOG_INFO, "Starting pi_webrtc for {$clientId}.");
 		`/app/bodycam2/camera/stream/pi_webrtc --use_libcamera --fps={$data['fps']} --width={$data['width']} --height={$data['height']} --hw_accel --no_audio --mqtt_host={$data['server']} --mqtt_port={$data['port']} --mqtt_username={$data['username']} --mqtt_password={$data['password']} --uid={$clientId}  >> /tmp/pi_webrtc.log 2>&1 &`;
+
+		// reset kill counter
+		$k = 0;
 	}
 	// it could run but be a zombie
 	else {
 		$status = trim(`/usr/bin/cat /proc/{$pid}/status | /usr/bin/grep "State:"`);
 
 		if (preg_match("/State:\s+Z/", $status)) {
-			echo date('r') . "> Zombie process {$pid}. Killing it.\n";
+			// try to kill zombie process
+			syslog(LOG_INFO, "Zombie process {$pid}. Killing it...");
 			`/usr/bin/kill -9 $pid`;
+
+			$k++;
+
+			if ($k > 5) {
+				// no dice, we have to reboot
+				syslog(LOG_INFO, "Unable to kill zombie process. Rebooting...");
+				`/usr/bin/reboot`;
+			}//if
 		}//if
 	}//if
 
