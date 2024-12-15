0b2c985a40d40296c4cb90527e994171ea22093e
Marek Drugac <marek@luceon.us>
2024-12-15 16:00:46 +0100

    dev

--- locomotive/web/src/views/map.php
+++ locomotive/web/src/views/map.php
@@ -2,7 +2,7 @@
 <div class="w-full h-screen overflow-hidden relative">
     <div id="map" class="w-full h-full z-5"></div>
 
-    <button id="btn-reset" class="absolute z-10 btn btn-dark" style="display: none; top: 15px; right: 85px;">
+    <button id="btn-reset" class="absolute z-10 btn btn-dark" style="display: none; bottom: 25px; right: 15px;">
         Reset
         <span class="filter-active d-none position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle">
             <span class="visually-hidden">New alerts</span>
