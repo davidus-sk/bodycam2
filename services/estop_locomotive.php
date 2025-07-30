be2f09bf0035205dcada4766b3f60eb9ea43f6fa
davidus-sk <travel@davidus.sk>
2025-07-30 16:24:03 -0400

    Create estop_locomotive.php

---
+++ services/estop_locomotive.php
@@ -0,0 +1,18 @@
+[Unit]
+Description=E-STOP MQTT Monitoring Service (locomotive listener)
+After=multi-user.target
+Wants=multi-user.target
+
+[Service]
+Type=simple
+ExecStart=/app/bodycam2/locomotive/scripts/button.php
+Restart=always
+RestartSec=1
+User=root
+WorkingDirectory=/app/bodycam2/locomotive/scripts
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=estop-locomotive-service
+
+[Install]
+WantedBy=multi-user.target
