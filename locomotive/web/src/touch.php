5d3e58769e2f7fc6931cfce74944f3ba7b28f77f
Marek Drugac <marek@luceon.us>
2025-08-08 17:57:10 +0200

    dev

--- locomotive/web/src/touch.php
+++ locomotive/web/src/touch.php
@@ -1,3 +1,16 @@
 <?php
 
-touch('./assets');
+$dir = realpath(__DIR__ . '/assets');
+
+$oldDatetime = filemtime($dir);
+
+touch($dir);
+clearstatcache();
+
+$newDatetime = filemtime($dir);
+
+echo '<div style="font-family: monospace;">';
+echo 'old filemtime: ' . date('Y-m-d H:i:s', $oldDatetime) . '<br>';
+echo 'new filemtime: ' . date('Y-m-d H:i:s', $newDatetime) . '<br>';
+echo 'ok';
+echo '</div>';
